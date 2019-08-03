a9a047568f9ee52c21bf711409f941b608e945f6
younes313 <44572345+younes313@users.noreply.github.com>
2019-08-03 16:25:44 +0430

    w

--- bazyaft/bazyaft/urls.py
+++ bazyaft/bazyaft/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
+    path('user/', include('user.urls') ),
     path('driver/' , include('driver.urls')) ,
 
 ]
